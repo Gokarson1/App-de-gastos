2321d122cafbe56675c5ed7ed832dd6d60bc046e
Gokarson1 <fabiansanhueza.ga@gmail.com>
2025-04-25 19:41:03 -0400

    firebase cositas xd

--- app_gastos/android/app/build.gradle.kts
+++ app_gastos/android/app/build.gradle.kts
@@ -3,12 +3,15 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
+
 }
 
 android {
+
     namespace = "com.example.app_gastos"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "29.0.13113456"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -42,3 +45,4 @@ android {
 flutter {
     source = "../.."
 }
+
